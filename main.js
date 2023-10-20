33280fc040cd80a51bbfc44fc5b776d19f4448e2
Noach Chazzan <146024296+noachchazzan@users.noreply.github.com>
2023-10-20 14:05:05 -0400

    minor changes

--- main.js
+++ main.js
@@ -78,47 +78,74 @@ const CourseInfo = {
   
   function getLearnerData(course, ag, submissions) {
     try {
-    if (course.id != ag.course_id) {
-      throw new Error("Invalid input: AssignmentGroup does not belong to course");
+    // Step 1: Validate that AssignmentGroup belongs to the course.
+      if (course.id != ag.course_id) {
+        throw new Error("Invalid input: AssignmentGroup does not belong to course");
     }
     // here, we would process this data to achieve the desired result.
-    // check if there is proper course_id with respect to 
+    // check if there is proper course_id with respect to the data.
+
+    // Step 2: Initialize variables. 
     let learners = [];
     let results = [];
+
+    // Step 3: Identify unique learners from the submissions. 
     for (let i in submissions) {
         if (!learners.includes(submissions[i].learner_id)){
             learners.push(submissions[i].learner_id)
-        } else {continue;}
+        } else {continue;} // Continue requirment. 
     }
+
+    // Step 4. Process data for each learner. 
     for (let learnerId of learners) {
+      // Step 4.1: Get all submissions for each learner of the learners array we built above.
       let learnersSubmissions = getLearnerSubmissions(learnerId, submissions);
+      // Step 4.2: Intialize result object for the learner. 
       let learnerResult = {
         id: learnerId,
         avg: 0
       };
+
+      // Step 4.3: Intialize total weight variable. 
       let totalWeight = 0;
+
+      // Step 4.4: Process each submission.
       for (let submission of learnersSubmissions) {
+        // Step 4.4.1: Get assignment info. Note we have a list of submissions but lack info on the assignments.
         let assignment = getAssignmentInfoById(submission.assignment_id, ag);
-        if (assignment && new Date(assignment.due_at) < new Date()) { // Checks for future due dates
+        // Step 4.4.2: Check if assignment is due. We now have the assignment info AND our submission with respect to the assignent's ID AND the student!
+        if (assignment && new Date(assignment.due_at) < new Date()) { // Compares due dates with current date which suggest this program reflects reality to a degree.
+          // Step 4.4.3: Get score and apply late penalty if needed (assignment info and submissions).
           let score = submission.submission.score;
           if (isSubmissionLate(submission.submission.submitted_at, assignment.due_at)) {
             score = applyLatePenalty(score, assignment.points_possible);
           }
+
+          // Step 4.4.4: Calculate score percentage.
           let scorePercentage = calculateAssignmentScore(score, assignment.points_possible);
-          learnerResult[assignment.id] = parseFloat(scorePercentage.toFixed(3)); // formats scores % to 1 decimal point
+
+          // Step 4.4.5: Update result object.
+          learnerResult[assignment.id] = parseFloat(scorePercentage.toFixed(3)); // formats scores
           learnerResult.avg += scorePercentage * assignment.points_possible;
+          // 4.4.6: Update total weight.
           totalWeight += assignment.points_possible;
         }
       }
+      
+      // Step 4.5: Calculate average score using total weight. 
       if (totalWeight > 0) {
         learnerResult.avg = parseFloat(learnerResult.avg / totalWeight).toFixed(3);
       }
+      // Step 4.6: Add result object to results array.
       results.push(learnerResult);
     }
+
+    // Step 5: Returns results.
     return results;
-  } catch(e){
+  } catch(e) {
+      // Handles errors if any 
       console.error(error.message);
-      return null;
+      return null; // If assignment with given ID is not found in AssignmentGroup then return null.
     }
   }
   
@@ -146,7 +173,7 @@ function getAssignmentInfoById(assignmentId, AssignmentGroup) {
 }
 
 function calculateAssignmentScore(submissionScore, assignmentMaxScore) {
-    return (submissionScore/assignmentMaxScore);
+    return submissionScore/assignmentMaxScore;
 }
 
 function isSubmissionLate(submitted_at, due_at) {
